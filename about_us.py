bb58c5bc6d72697bd800ae0082e69132ed082db6
manhmanh39 <148619729+manhmanh39@users.noreply.github.com>
2024-10-31 10:37:58 +0700

    Update about_us.py

--- about_us.py
+++ about_us.py
@@ -4,7 +4,7 @@ def display_about_info():
     st.title("About Us - CALM")
     st.write("Bạn có bao giờ đứng trước gian bếp với hàng tá nguyên liệu mà không biết làm món gì? Chào mừng đến với CALM, nơi bạn có thể tìm thấy vô số công thức nấu ăn đa dạng. Mỗi công thức đều được trình bày chi tiết, kèm theo hình ảnh minh họa và mẹo nhỏ giúp bạn dễ dàng thực hiện dù là lần đầu vào bếp. Đặc biệt, với giao diện thân thiện và các công cụ tìm kiếm tiện lợi, bạn có thể chọn món theo nguyên liệu và tình trạng sức khỏe. Hãy để mỗi bữa ăn trở thành một trải nghiệm thư giãn và khám phá từng món ăn mỗi ngày cùng chúng tôi!")
 
-st.header("Our Sloogan |n_n|")
+st.header("Our Slogan |n_n|")
 st.write('"Health - Nutrition - Happiness"')
 st.write("Sứ mệnh của CALM là truyền cảm hứng để mọi người trải nghiệm bữa ăn lành mạnh, đầy đủ dinh dưỡng và tràn ngập niềm vui. Chúng tôi luôn hướng đến các công thức chế biến theo cách tối ưu nhất để giữ trọn giá trị dinh dưỡng. Mỗi món đều cung cấp đủ dưỡng chất cần thiết, đảm bảo sự cân bằng giữa hương vị và sức khỏe. Không chỉ là những món ăn, chúng tôi mong muốn bạn sẽ tìm thấy niềm vui, sự thư giãn và cảm giác hạnh phúc khi tự tay chế biến và thưởng thức những bữa ăn bổ dưỡng bên gia đình.")
 
